8fefcfa625d722930dd6cfb0f8a17aa71335a676
MoltenEgg <MoltenEgg97@gmail.com>
2025-11-08 11:33:27 +0400

    working project

--- RockPapperScissors/RockPapperScissors.py
+++ RockPapperScissors/RockPapperScissors.py
@@ -1,1 +1,42 @@
-print ('Hello')
+import random
+
+moves = ['rock', 'papper', 'scissors']
+
+game_is_finished = False
+
+def game_ended(has_a_winner):
+    if has_a_winner == True: return True
+    return False
+
+
+
+def check_moves(player1_move, player1, player2_move, player2):
+    made_moves = {
+        player1_move: player1,
+        player2_move: player2
+    }
+    if 'papper' in made_moves and 'rock' in made_moves:
+        print (f' победил {made_moves['papper']}')
+        return True
+    if 'rock' in made_moves and 'scissors' in made_moves:
+        print (f' победил {made_moves['rock']}')
+        return True
+    if 'papper' in made_moves and 'scissors' in made_moves:
+        print (f' победил {made_moves['scissors']}')
+        return True
+    if player1_move == player2_move:
+        print ('ничья')
+        return False
+    
+while game_ended(game_is_finished) == False:
+    print ('Игрок 1 введите rock, papper, or scissors')
+    player_move = input(str).lower()
+    while player_move not in moves:
+        print ('неверный ход')
+        player_move = input(str) 
+    print (f'игрок1 ввел {player_move}')
+
+    pc_move = moves[random.randint(0, 2)]
+    print (f' компьютер ввел {pc_move}')
+
+    game_is_finished = check_moves(player_move, 'игрок', pc_move, 'компьютер')
