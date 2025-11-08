98f5570cf0016d60e701b6e4de4ed174857ec7d5
MoltenEgg <MoltenEgg97@gmail.com>
2025-11-08 17:18:18 +0400

    Удалено game_ended() и исправлены опечатки

--- RockPapperScissors/RockPapperScissors.py
+++ RockPapperScissors/RockPapperScissors.py
@@ -1,13 +1,8 @@
 import random
 
-moves = ['rock', 'papper', 'scissors']
+moves = ['rock', 'paper', 'scissors']
 
 game_is_finished = False
-
-def game_ended(has_a_winner):
-    if has_a_winner == True: return True
-    return False
-
 
 
 def check_moves(player1_move, player1, player2_move, player2):
@@ -15,28 +10,28 @@ def check_moves(player1_move, player1, player2_move, player2):
         player1_move: player1,
         player2_move: player2
     }
-    if 'papper' in made_moves and 'rock' in made_moves:
-        print (f' победил {made_moves['papper']}')
+    if 'paper' in made_moves and 'rock' in made_moves:
+        print (f' победил {made_moves['paper']}')
         return True
     if 'rock' in made_moves and 'scissors' in made_moves:
         print (f' победил {made_moves['rock']}')
         return True
-    if 'papper' in made_moves and 'scissors' in made_moves:
+    if 'paper' in made_moves and 'scissors' in made_moves:
         print (f' победил {made_moves['scissors']}')
         return True
     if player1_move == player2_move:
         print ('ничья')
         return False
     
-while game_ended(game_is_finished) == False:
-    print ('Игрок 1 введите rock, papper, or scissors')
+while game_is_finished == False:
+    print ('Игрок 1 введите rock, paper, or scissors')
     player_move = input(str).lower()
     while player_move not in moves:
         print ('неверный ход')
         player_move = input(str) 
     print (f'игрок1 ввел {player_move}')
 
-    pc_move = moves[random.randint(0, 2)]
-    print (f' компьютер ввел {pc_move}')
+    pc_move = random.choice(moves)
+    print (f'компьютер ввел {pc_move}')
 
     game_is_finished = check_moves(player_move, 'игрок', pc_move, 'компьютер')
